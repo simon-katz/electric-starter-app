2d81d8506e13868029c973bcfdf2ae02b142a166
Simon Katz <sk@nomistech.com>
2023-07-29 20:36:22 +0100

    Add some notes

--- src/app/server/stuff.clj
+++ src/app/server/stuff.clj
@@ -15,9 +15,12 @@
 ;;       `app.todo-list` namespace?
 ;;       - Oh! See the binding of `db` in `app.todo-list`.\
 ;;       - `e/watch` creates a reactive value from a reference.
+;;         - The DB implementation using an atom makes things easier
+;;           to understand, because you know how atoms work and you
+;;           don't have to think about where the magic lives.
 
 ;; TODO: Where is the datascript dependency coming from? Not in deps.edn, right?
-;;       Is it part of Electric? (That would be weird, right?)
+;;       - Electric has it as a dependency. Is that weird?
 
 ;; TODO: Copy this to a new repo where the DB is a set of rules and
 ;;       events, and have at it!
