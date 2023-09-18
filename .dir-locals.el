37d74d38f42ef7b2fdc6d0c4225f5fff6acdc168
Simon Katz <sk@nomistech.com>
2023-09-18 14:56:03 +0100

    Add `:test` to `cider-clojure-cli-aliases`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((nil
   .
-  ((cider-clojure-cli-aliases . ":dev")
+  ((cider-clojure-cli-aliases . ":dev:test")
    (cider-preferred-build-tool . clojure-cli)
    ;; If you don't have `nomis/add-to-list-local`, you can copy it from
    ;; https://tinyurl.com/nomis-vgsn-el (or, if it's no longer there,
